50ba4e0dbfdd7e95be1ad90f688687a025178592
gds25 <70597132+gds25@users.noreply.github.com>
2020-11-03 17:23:04 -0500

    Create JavaData.java

---
+++ Homework/JavaData.java
@@ -0,0 +1,27 @@
+
+public class JavaData {
+
+		public static void main(String[] args) {
+
+			// 1. Create a variable that holds a name
+			String name = "Gabriel";
+			
+			// 2. Output the name to the console
+			System.out.println(name);
+			
+			// 3. Create a variable that holds an age (as a whole number)
+			int age = 18;
+			
+			// 4. Output the age to the console
+			System.out.println(age);
+
+			// 5. Create a variable that holds a decimal
+			double decimal = 1.11;
+			
+			// 6. Output the variable
+			System.out.println(decimal);
+		}
+
+
+
+}
